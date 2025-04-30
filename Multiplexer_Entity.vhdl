60849e93bfd30b5aae90234fd167e34a89a50f8f
SaadKasu <saadkasu@protonmail.com>
2025-04-30 22:34:10 +0200

    Fixed Output Name

--- Multiplexer_Entity.vhdl
+++ Multiplexer_Entity.vhdl
@@ -3,18 +3,18 @@ USE ieee.std_logic_1164.all;
 
 ENTITY multiplexer IS
     PORT (dIN1, dIN2, ch : IN std_logic;
-          out            : OUT std_logic);
+          output         : OUT std_logic);
 
 END multiplexer; 
 
 ARCHITECTURE multiplexer_Behavior of multiplexer is
     BEGIN
-        PROCESS (dIN1, dIN2, ch, out) is
+        PROCESS (dIN1, dIN2, ch, output) is
             BEGIN
                 IF (ch = '0') then
-                    out <= dIN1;
+                    output <= dIN1;
                 ELSE
-                    out <= dIN2;
+                    output<= dIN2;
             END IF;
         END PROCESS;
 END multiplexer_Behavior;
